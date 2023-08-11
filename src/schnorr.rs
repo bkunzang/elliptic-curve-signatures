4d092c89275a2d83c91c7278ce17b8fbe69b3a7d
bkunzang <broob@blui.com>
2023-08-11 16:14:05 -0700

    add comments

--- src/schnorr.rs
+++ src/schnorr.rs
@@ -15,30 +15,44 @@ pub trait SchnorrGroup {
 impl<T: Group + GroupEncoding> SchnorrGroup for T {
     type Scalar = <Self as Group>::Scalar;
     fn generate_private_key() -> Self::Scalar {
+        // generate random scalar
         let rng = rand::thread_rng();
         <Self::Scalar as Field>::random(rng)
     }
 
     fn generate_public_key(sk: Self::Scalar) -> T {
+        // public key = secret key * generator
         Self::generator() * sk
     }
 
     fn sign(sk: Self::Scalar, message: &str) -> (Self::Scalar, Self) {
+        // r is a random scalar to be regenerated for each signature
         let r = Self::generate_private_key();
+        // r_point = r * generator
         let r_point = Self::generate_public_key(r);
+        // convert to bytes for hashing
         let r_point_bytes = r_point.to_bytes();
         let pk = Self::generate_public_key(sk).to_bytes();
+
         let inputs = vec![message.as_bytes(), pk.as_ref(), r_point_bytes.as_ref()];
         let hash = hash::<T>(inputs);
+
         let s = r + sk * hash;
+        // signature is s and r_point
         (s, r_point)
     }
 
     fn verify(signature: (Self::Scalar, Self), pk: Self, message: &str) -> bool {
         let (s, r_point) = signature;
+        // convert to bytes for hashing
         let (pk_bytes, r_point_bytes) = (pk.to_bytes(), r_point.to_bytes());
-        let inputs = vec![message.as_bytes(), pk_bytes.as_ref(), r_point_bytes.as_ref()];
+        let inputs = vec![
+            message.as_bytes(),
+            pk_bytes.as_ref(),
+            r_point_bytes.as_ref(),
+        ];
         let hash = hash::<T>(inputs);
+        // s * G = (r + sk*hash) * G = (r * G) + (sk * G * hash) = r_point + pk * hash
         return Self::generator() * s == (r_point + pk * hash);
     }
 }
@@ -64,6 +78,7 @@ mod test {
         }
     }
 
+    // generates random string of characters to test signatures 
     fn get_random_message(length: usize) -> String {
         let message: Vec<char> = rand::thread_rng()
             .sample_iter(&Alphanumeric)
@@ -74,6 +89,7 @@ mod test {
         message2
     }
 
+    // passes unaltered randomly generated signature and message through verifier and checks if it returns true
     fn schnorr_test_true_aux() {
         let sk = ProjectivePoint::generate_private_key();
         let pk = ProjectivePoint::generate_public_key(sk);
@@ -84,6 +100,7 @@ mod test {
         assert_eq!(verifier, true);
     }
 
+    // passes altered message through verifier and checks if it returns false
     fn schnorr_test_false_aux() {
         let sk = ProjectivePoint::generate_private_key();
         let pk = ProjectivePoint::generate_public_key(sk);
