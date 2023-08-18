b2d02c409fb756c6be1cf25d13c48dd032d2dbe5
bkunzang <broob@blui.com>
2023-08-18 16:45:11 -0700

    Add comments.

--- src/ecdh.rs
+++ src/ecdh.rs
@@ -1,5 +1,6 @@
 use elliptic_curve::{Field, Group, PrimeField};
 
+// Required functions for ECDH (elliptic curve Diffie Hellman exchange)
 pub trait ECDHGroup {
     type Scalar: PrimeField;
 
@@ -13,15 +14,22 @@ pub trait ECDHGroup {
 impl<T: Group> ECDHGroup for T {
     type Scalar = T::Scalar;
 
+    // Private key is a random scalar
     fn generate_private_key() -> Self::Scalar {
         let rng = rand::thread_rng();
         <Self::Scalar as Field>::random(rng)
     }
-
+    // Pk = sk * generator (in this case an elliptic curve point)
     fn generate_public_key(sk: Self::Scalar) -> Self {
         Self::generator() * sk
     }
 
+    // If parties A and B both have private and public keys, they can generate a shared secret as follows:
+    // Secret of A: pk_b * sk_a
+    // Secret of B: pk_a * sk_b
+    // pk_b = sk_b * generator
+    // pk_a = sk_a * generator
+    // Therefore, both secrets equal sk_b * sk_a * generator and are equal
     fn generate_secret(sk: Self::Scalar, pk: Self) -> Self {
         pk * sk
     }
@@ -39,6 +47,7 @@ mod test {
         }
     }
 
+    // Generate a two random signers and create a shared secret, then check that the shared secrets are equal.
     fn ecdh_test_aux() {
         let sk_a = ProjectivePoint::generate_private_key();
         let pk_a = ProjectivePoint::generate_public_key(sk_a);
