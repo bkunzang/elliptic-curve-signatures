91b1c1188aac8908ef3004cf83a224ffef5ea818
Brian Künzang <3960464+bkunzang@users.noreply.github.com>
2023-08-27 15:24:44 -0700

    ECDSA (#7)
    * sorry for the mess
    
    * use extension traits
    
    * tests/implementation for k256 projective pt
    
    * progress on ecdsa
    
    * fix up hash function
    
    * added point coordinate scalar type
    
    * Implement convert.
    
    * Eliminate bits reversal.
    
    * Eliminate intermediate bits.
    
    * refactor ecdsa hash
    
    * refactor ecdsa hash
    
    * Get x.
    
    * some work on ecdsa
    
    * Refactor hash/add comments
    
    * add more comments
    
    * Formatting
    
    * Add doc comments
    
    ---------
    
    Co-authored-by: Matej Penciak <matej.penciak@gmail.com>
    Co-authored-by: porcuquine <porcuquine@users.noreply.github.com>

--- src/ecdsa.rs
+++ src/ecdsa.rs
@@ -1,66 +1,78 @@
-use elliptic_curve::{Field, Group, PrimeField};
+use elliptic_curve::{group::GroupEncoding, Field, Group, PrimeField};
+use k256::ProjectivePoint;
 use sha2::{Digest, Sha256};
 
+/// Requires the implementations of methods to create ECDSA signatures
 pub trait ECDSAGroup {
+
+    /// Scalars modulo the order of the elliptic curve group
     type Scalar: PrimeField;
 
+    /// Generate random scalar that can be used as a private key
     fn generate_private_key() -> Self::Scalar;
 
+    /// Generate public key given secret/private key: pk = sk * generator
     fn generate_public_key(sk: Self::Scalar) -> Self;
+    
+    /// Create an ECDSA signature given a signer's secret key and a message to sign
+    fn sign(secret_key: Self::Scalar, message: &[u8]) -> (Self::Scalar, Self::Scalar);
 
-    fn sign(secret_key: Self::Scalar, message: &str) -> (Self::Scalar, Self::Scalar);
-
-    fn verify(signature: (Self::Scalar, Self::Scalar), message: &str, public_key: Self) -> bool;
+    /// Verify a message signed using ECDSA; returns true/false depending on if the signature is valid
+    fn verify(signature: (Self::Scalar, Self::Scalar), message: &[u8], public_key: Self) -> bool;
 }
 
-pub trait CurveGroup: Group {
-    type PointScalar: PrimeField;
-    fn x(self) -> Self::PointScalar;
-    fn y(self) -> Self::PointScalar;
-    fn z(self) -> Self::PointScalar;
+/// Provides methods to extract the x coordinate from an elliptic curve point and convert that coordinate into an element of the scalar field.
+pub trait CurveGroup: Group + GroupEncoding {
+    /// Function to extract the x coordinate of an elliptic curve point as bytes. This function must be implemented for any curve that you are using with this code (see below for an example with k256::ProjectivePoint)
+    fn x(&self) -> Vec<u8>;
 
-    fn convert(mut x: Self::PointScalar) -> Self::Scalar {
-        let mut binary_place = Self::Scalar::ONE;
+    /// Take bytes of x coordinate and convert to a scalar
+    fn convert(&self) -> Self::Scalar {
+        let x_bytes = self.x();
+
+        let mut place = Self::Scalar::ONE;
         let mut res = Self::Scalar::ZERO;
 
-        for _ in 0..Self::PointScalar::NUM_BITS {
-            if x.is_odd().into() {
-                x = x - Self::PointScalar::ONE;
-                res += binary_place;
-            }
+        let mut buf = [0u8; 16];
 
-            x = x * Self::PointScalar::TWO_INV;
-            binary_place = binary_place.double();
+        let mut factor = Self::Scalar::ONE;
+        for _ in 0..128 {
+            factor = factor.double();
+        }
+
+        for chunk in x_bytes.chunks(16) {
+            buf.copy_from_slice(chunk);
+            let num = u128::from_le_bytes(buf);
+            let scalar = Self::Scalar::from_u128(num);
+            res += place * scalar;
+
+            place *= factor;
         }
         res
     }
 }
 
-// Hash function is incomplete
-fn hash<T: Group>(message: &str) -> T::Scalar {
+/// Hash a message to be signed and return a scalar.
+pub fn ecdsa_hash<T: Group>(input: &[u8]) -> T::Scalar {
     let mut hasher = Sha256::new();
-    hasher.update(message);
+    hasher.update(input);
     let hash = hasher.finalize();
+    let mut factor = T::Scalar::ONE;
+    // Create a scalar with value 2^8
+    for _ in 0..8 {
+        factor = factor.double()
+    }
     let mut scalar = <T::Scalar as Field>::ZERO;
+    // Construct a scalar from each byte
     for byte in hash {
-        // TODO: Maybe this is big endian instead of little endian?
-        scalar *= <T::Scalar as From<u64>>::from(256); // TODO: Maybe do this by doubling?
+        scalar *= factor;
         scalar += <T::Scalar as From<u64>>::from(byte as u64)
     }
-    // let mut scalars: Vec<T::Scalar> = Vec::new();
-    // let a: u128;
-    // for i in hash.chunks(16) {
-    //     let bytes_array = i.try_into().unwrap();
-    //     let a = u128::from_le_bytes(bytes_array);
-    //     let a = PrimeField::from_u128(a);
-    //     scalars.push(a);
-    // }
-    //Todo: create scalar from u128s/bytes
-    scalar // TODO: Generate test vectors from the Sage script and compare with this output
+    scalar
 }
-// int.from_bytes(hash)
-impl<T: CurveGroup + Group> ECDSAGroup for T {
-    type Scalar = <T as Group>::Scalar;
+
+impl<T: CurveGroup> ECDSAGroup for T {
+    type Scalar = T::Scalar;
     fn generate_private_key() -> Self::Scalar {
         let rng = rand::thread_rng();
         <Self::Scalar as Field>::random(rng)
@@ -70,44 +82,123 @@ impl<T: CurveGroup + Group> ECDSAGroup for T {
         Self::generator() * sk
     }
 
-    fn sign(sk: Self::Scalar, message: &str) -> (Self::Scalar, Self::Scalar) {
+    fn sign(sk: Self::Scalar, message: &[u8]) -> (Self::Scalar, Self::Scalar) {
         // Need to take leftmost bits of z, todo (also in verifier)
-        let z: Self::Scalar = hash::<Self>(message);
+        let z: Self::Scalar = ecdsa_hash::<T>(message);
         let mut rng = rand::thread_rng();
         let k = <Self::Scalar as Field>::random(&mut rng);
         // Check that k != 0
         assert!(k.is_zero().unwrap_u8() == 0);
         let point = Self::generator() * k;
-        let r = <Self as CurveGroup>::convert(point.x());
+        //let point_bytes = point.to_bytes();
+        let r = point.convert();
+        //let r = <Self as CurveGroup>::convert(x_bytes);
         let s = k.invert().unwrap() * (z + r * sk);
-        return (r, s);
+        (r, s)
     }
 
-    fn verify(signature: (Self::Scalar, Self::Scalar), message: &str, public_key: Self) -> bool {
-        assert!(public_key != T::identity());
+    fn verify(signature: (Self::Scalar, Self::Scalar), message: &[u8], public_key: Self) -> bool {
+        assert!(public_key != Self::identity());
         let (r, s) = signature;
-        let z: Self::Scalar = hash::<Self>(message);
+        let z: Self::Scalar = ecdsa_hash::<T>(message);
         let s_inv = s.invert().unwrap();
         let u1 = z * s_inv;
         let u2 = r * s_inv;
         let point = Self::generator() * u1 + public_key * u2;
-        return <Self as CurveGroup>::convert(point.x()) == r;
+        assert!(point != Self::identity());
+        point.convert() == r
     }
 }
 
-//#[cfg(test)]
-//use super::*;
-//use k256::ProjectivePoint;
-//
+impl CurveGroup for ProjectivePoint {
+    fn x(&self) -> Vec<u8> {
+        let bytes = self.to_bytes();
 
-//impl CurveGroup for ProjectivePoint {
-//type PointScalar = ProjectivePoint::FieldElement;
-//fn x(self) -> self.x;
-// fn y(self) -> self.y;
-//  fn z(self) -> self.z;
-//}
-//fn ecdsa_test_aux() {
-//let sk = ProjectivePoint::generate_private_key();
-//let pk = ProjectivePoint::generate_public_key(sk);
-//  let message = rand::thread_rng().sample_iter(&Alphanumeric).take(10).map(char::from).collect();
-//}
+        let bytes: &[u8] = bytes.as_ref();
+
+        let size = 32;
+
+        // NOTE: this is a hack that depends on the k256 implementation so probably should not be
+        // Curve points are stored as an x coordinate with a tag to indicate sign; the sign is the first byte and the x coordinate is the remaining 32 bytes.
+        assert_eq!(bytes.as_ref().len(), 1 + size);
+        let (start, end) = (1, 1 + size);
+
+        let mut res = vec![0u8; size];
+
+        res.copy_from_slice(&bytes[start..end]);
+        res
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use k256::ProjectivePoint;
+    use rand::distributions::Alphanumeric;
+    use rand::Rng;
+
+    #[test]
+    fn ecdsa_test_true() {
+        for _ in 1..100 {
+            ecdsa_test_true_aux()
+        }
+    }
+
+    #[test]
+    fn ecdsa_test_false() {
+        for _ in 1..100 {
+            ecdsa_test_false_aux()
+        }
+    }
+
+    /// Generates a random signer (public and private keys)
+    fn generate_random_signer() -> (<ProjectivePoint as Group>::Scalar, ProjectivePoint) {
+        let sk = ProjectivePoint::generate_private_key();
+        let pk = ProjectivePoint::generator() * sk;
+
+        (sk, pk)
+    }
+
+    /// Generates a random string to use as a message in tests
+    fn get_random_message(length: usize) -> String {
+        let message: Vec<char> = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(length)
+            .map(char::from)
+            .collect();
+        let message2 = message.into_iter().collect::<String>();
+        message2
+    }
+
+    /// Tests whether a random message verifies correctly for a random signer
+    fn ecdsa_test_true_aux() {
+        let (sk, pk) = generate_random_signer();
+
+        let message = get_random_message(10);
+        let message_bytes = message.as_bytes();
+
+        let signature = ProjectivePoint::sign(sk, message_bytes.as_ref());
+
+        let verifier = ProjectivePoint::verify(signature, message_bytes.as_ref(), pk);
+
+        assert_eq!(verifier, true);
+    }
+
+    /// Tests whether a random message replaced by a random message with a different length (cannot be the same as the original) correctly fails to verify.
+    fn ecdsa_test_false_aux() {
+        let (sk, pk) = generate_random_signer();
+
+        let message = get_random_message(10);
+        let message_bytes = message.as_bytes();
+        
+        //Create 11 character message that must be different from the original message
+        let message_altered = get_random_message(11);
+        let message_altered_bytes = message_altered.as_bytes();
+
+        let signature = ProjectivePoint::sign(sk, message_bytes.as_ref());
+
+        let verifier = ProjectivePoint::verify(signature, message_altered_bytes.as_ref(), pk);
+
+        assert_eq!(verifier, false);
+    }
+}
